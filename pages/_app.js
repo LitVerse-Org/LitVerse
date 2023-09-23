cfbe4d8e4309082ea63ba77d09ce94ce8074b2f1
Dobson Dunavant <dobsonddev@gmail.com>
2023-09-23 16:27:00 -0400

    Prisma/PSQL Integration, Full DB Refactor, G and FB OAuth Setup, AWS S3 Connection and setup
    Prisma/PSQL Integration, Full Database Refactor from MongoDB, Google and FaceBook OAuth Setup, AWS S3 Connection and setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,28 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import { SessionProvider } from "next-auth/react";
-import TimeAgo from 'javascript-time-ago'
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en.json';
 
-import en from 'javascript-time-ago/locale/en.json'
+// Import Amplify and your AWS configuration
+import Amplify from 'aws-amplify';
+import awsmobile from '../aws-exports'; // Replace with the correct path to your aws-exports.js file
 
 TimeAgo.addDefaultLocale(en);
 
+// Configure Amplify with your AWS settings
+Amplify.configure(awsmobile);
+console.log(awsmobile.aws_project_region); // Example: 'us-east-1'
+console.log(awsmobile.aws_s3_buckets.profilePicsBucket); // Example: 'litverse-profile-pics-bucket'
 
 function MyApp({
-   Component,
-   pageProps: { session, ...pageProps },
-}) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+                   Component,
+                   pageProps: { session, ...pageProps },
+               }) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,20 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID, // Your Facebook App ID
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET, // Your Facebook App Secret
     }),
   ],
   pages: {
@@ -18,7 +24,28 @@ export const authOptions = {
     strategy: 'jwt',
   },
   callbacks: {
-    session: async ({token,session}) => {
+    async signIn(user, account, profile) {
+      const existingUser = await prisma.user.findFirst({
+        where: {
+          email: user.email,
+        },
+      });
+
+      if (existingUser) {
+        return true;
+      }
+
+      await prisma.user.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          // ... other fields you want to include
+        },
+      });
+
+      return true;
+    },
+    async session(session, token) {
       if (session?.user && token?.sub) {
         session.user.id = token.sub;
       }
