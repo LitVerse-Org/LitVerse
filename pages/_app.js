9e79c5e6da9d3fd9a3a3a0b6fac8a1d8d6ffbf6c
Dobson Dunavant <dobsonddev@gmail.com>
2023-09-30 20:51:42 -0400

    removed mongoose dependencies and aws-amplify from the project

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,10 @@ import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en.json';
-import Amplify from 'aws-amplify';
-import awsExports from '../aws-exports';  // Replace with the correct path to your aws-exports.js file
+import de from 'javascript-time-ago/locale/de.json'; // Import other locales as needed
 
 TimeAgo.addDefaultLocale(en);
-
-
-// Keep the below code commented out, as it will cause an error when you run the app. We'll uncomment it later if I can ever figure out this .configure() bug.
-// console.log(Amplify);  // Should output the Amplify object
-// console.log(awsExports);  // Should output your AWS settings
-// Amplify.configure({ ...awsExports, ssr: true });  // Use awsExports here
+TimeAgo.addLocale(de); // Add other locales as needed
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
     return (
