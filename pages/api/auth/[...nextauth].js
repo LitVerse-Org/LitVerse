a2371067e36138a17d0d4797614eb87d810f042e
Dobson Dunavant <dobsonddev@gmail.com>
2023-10-03 12:52:59 -0400

    me = silly, fixed mongoose bug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,30 +1,69 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
-
-import { PrismaClient } from '@prisma/client';
+import CredentialsProvider from "next-auth/providers/credentials";  // For email/password
+import { PrismaClient } from "@prisma/client";
+import validator from "validator";
+import bcrypt from 'bcrypt';  // For password hashing
 
 const prisma = new PrismaClient();
 
-export const authOptions = {
+const generateUniqueUsername = () => {
+  // Replace with your unique username logic
+  return "unique_username";
+};
+
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID, // Your Facebook App ID
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET, // Your Facebook App Secret
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign-in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const { username, password } = credentials;
+
+        // Check if username and password are valid
+        const user = await prisma.user.findFirst({ where: { username } });
+        if (user && await bcrypt.compare(password, user.password)) {
+          return { id: user.id, username: user.username, email: user.email };
+        } else {
+          return null;
+        }
+      }
     }),
   ],
   pages: {
     signIn: '/login',
   },
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   callbacks: {
     async signIn(user, account, profile) {
+      // For custom credential login
+      if (account.provider === 'credentials') {
+        if (user) {
+          return true;
+        }
+        return false;
+      }
+
+      // Validate email for social logins
+      if (!validator.isEmail(user.email)) {
+        return false;
+      }
+
       const existingUser = await prisma.user.findFirst({
         where: {
           email: user.email,
@@ -35,11 +74,17 @@ export const authOptions = {
         return true;
       }
 
+      // Hash password if this is a custom registration
+      let hashedPassword = null;
+      if (account.provider === 'credentials') {
+        hashedPassword = await bcrypt.hash(user.password, 10);
+      }
+
       await prisma.user.create({
         data: {
-          name: user.name,
           email: user.email,
-          // ... other fields you want to include
+          username: profile.username || generateUniqueUsername(),
+          password: hashedPassword,  // Save hashed password if this is a custom registration
         },
       });
 
@@ -52,6 +97,4 @@ export const authOptions = {
       return session;
     },
   },
-};
-
-export default NextAuth(authOptions);
+});
--- pages/login.js
+++ pages/login.js
@@ -1,22 +1,30 @@
 import { useState } from 'react';
-import { getProviders, signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { getProviders, signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function LoginPage({ providers }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [showModal, setShowModal] = useState(false);
   const { data, status } = useSession();
   const router = useRouter();
 
-  if (status === "authenticated") {
-    router.push("/");
+  if (status === 'authenticated') {
+    router.push('/');
   }
 
-  //PASSWORD RULES:
-  // Minimum length of 8 characters.
-  //     At least one uppercase letter.
-  //     At least one lowercase letter.
-  //     At least one number.
-  //     At least one special character (e.g., !, @, #, $, etc.).
+  const handleSignIn = async () => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      username,
+      password,
+    });
+    if (result.error) {
+      // Handle error (e.g., show a notification or message)
+    } else {
+      router.push('/');
+    }
+  };
 
   return (
       <div className="flex items-center justify-center h-screen rl-stripe-bg">
@@ -38,6 +46,8 @@ export default function LoginPage({ providers }) {
                 type="text"
                 placeholder="Username"
                 className="w-full p-3 rounded-full border border-blue-300"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -45,10 +55,16 @@ export default function LoginPage({ providers }) {
                 type="password"
                 placeholder="Password"
                 className="w-full p-3 rounded-full border border-blue-300"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="mb-2">
-            <button className="text-white p-2 rounded-full w-full" style={{ backgroundColor: '#373a3a' }}>
+            <button
+                className="text-white p-2 rounded-full w-full"
+                style={{ backgroundColor: '#373a3a' }}
+                onClick={handleSignIn}
+            >
               Sign In
             </button>
           </div>
@@ -58,7 +74,7 @@ export default function LoginPage({ providers }) {
             </button>
           </div>
           <div className="mb-2 text-center">
-            <button className="position-center text-black p-1 rounded-full w-56 bg-white" onClick={() => router.push('/register')}>
+            <button className="text-black p-1 rounded-full w-56 bg-white" onClick={() => router.push('/register')}>
               No Account? Register Here
             </button>
           </div>
@@ -115,9 +131,6 @@ export default function LoginPage({ providers }) {
               </div>
             </div>
         )}
-
-
-
       </div>
   );
 }
@@ -128,4 +141,3 @@ export async function getServerSideProps() {
     props: { providers },
   };
 }
-
