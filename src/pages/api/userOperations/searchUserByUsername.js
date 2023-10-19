d2705256b3aba31dd3c97b4643903cb30f7224a9
woodythenoob <68041244+woodythenoob@users.noreply.github.com>
2023-10-19 13:24:33 -0400

    grayers Contributions
    API:
    
    postOperations
    -getLikesForPost (gets the likes)
    -getUsersPosts (gets the posts by UID)
    -likePost (will like a post and update db)
    -unlinkePost (will unlike and update db)
    
    userOperations
    -searchUserbyUsername : will take uid and get the username
    -searchUsers : will use UID to get inforamoint
    
    Pages:
    userPosts - my testing page for the enviroment

---
+++ src/pages/api/userOperations/searchUserByUsername.js
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function searchUserByUsername(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).end(); // Method Not Allowed
+  }
+
+  const { username } = req.query;
+
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required' });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    return res.status(200).json({ userId: user.id });
+  } catch (error) {
+    console.error('Error searching user:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
---
+++ src/pages/api/userOperations/searchUsers.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function searchUsers(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).end(); // Method Not Allowed
+  }
+
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query is required' });
+  }
+
+  try {
+    const users = await prisma.user.findMany({
+      where: {
+        username: {
+          startsWith: query,
+        },
+      },
+      select: {
+        username: true,
+      },
+    });
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error('Error searching users:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
