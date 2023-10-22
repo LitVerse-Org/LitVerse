218e77a83dd5a1b0c5379226447bb70f4d18007a
Dobson Dunavant <dobsonddev@gmail.com>
2023-10-22 00:35:45 -0400

    OAuth2.0 stuff and some auth UI stuff

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,48 +3,43 @@ import { getSession, useSession, signOut } from "next-auth/react";
 import sidebar from "../components/sidebar";
 
 export default function Home() {
-    const handleLogout = async () => {
-        await signOut(); // Call the signOut function to log the user out
-    };
+    const { data: session, status } = useSession(); // Use the useSession hook here
 
-    const { data: session, status } = useSession();
+    const handleLogout = async () => {
+        await signOut();
+    };
 
     useEffect(() => {
         async function fetchData() {
-            const session = await getSession();
-            console.log("Current Session token in home.js page from `getSession()`: ", session);
+            const fetchedSession = await getSession();
+            console.log("Current Session token in home.js page from `getSession()`: ", fetchedSession);
         }
         fetchData();
         console.log("Session object from useSession:", session);
         console.log("Status from useSession:", status);
     }, []);
 
-
     return (
         <div>
-            {status === "loading" ? (
-                <p>Loading...</p>
-            ) : status === "authenticated" ? (
+            <div
+                style={{
+                    background: "pink",
+                    height: "100vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <div style={{ color: "green", fontSize: "2em" }}>HOME</div>
                 <div>
                     <p>
-                        {console.log('Rendering session.user:', session.user)}
-                        Signed in as {session.user?.email || session.user?.username || "Unknown"}
+                        {console.log('Rendering session.user:', session)}
+                        Signed in as {JSON.stringify(session)}
                     </p>
-                    <button onClick={handleLogout}>Logout</button>
+                    <button onClick={handleLogout} className="bg-red-800">Logout</button>
                 </div>
-            ) : (
-                <div
-                    style={{
-                        background: "pink",
-                        height: "100vh",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <span style={{ color: "green", fontSize: "2em" }}>HOME</span>
-                </div>
-            )}
+            </div>
+
         {/*<div style={{*/}
         {/*    background: "black",*/}
         {/*    height: "110vh",*/}
