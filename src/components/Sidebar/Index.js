7070a085910156f48d9328ada1128783bd45ff92
Dobson Dunavant <dobsonddev@gmail.com>
2023-10-23 17:56:53 -0400

    updated sidebar component routing

--- src/components/Sidebar/Index.js
+++ src/components/Sidebar/Index.js
@@ -1,54 +1,48 @@
 import React, { useState } from 'react';
 import styles from './sidebar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { 
-  faHome,  
-  faBell, 
-  faBookmark, 
-  faCog, 
-  faPen, 
-  faUser, 
-  faEnvelope 
-} from '@fortawesome/free-solid-svg-icons'; 
+import { useRouter } from 'next/router';
+import {
+    faHome,
+    faBell,
+    faBookmark,
+    faCog,
+    faPen,
+    faUser,
+    faEnvelope
+} from '@fortawesome/free-solid-svg-icons';
 import SettingsModal from './SettingsModal';
 import Link from 'next/link';
 
 const Sidebar = () => {
     const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
+    const router = useRouter(); // Declare it here
 
     const toggleSettingsModal = () => {
         setIsSettingsModalOpen(!isSettingsModalOpen);
     };
 
-    // No need for handleProfileIconClick function as we are directly using the Link component
-
     return (
         <div className={styles.sidebarContainer}>
-            <FontAwesomeIcon icon={faHome} className={styles.sidebarItem} />  
-            <FontAwesomeIcon icon={faBell} className={styles.sidebarItem} /> 
-            <span 
-            className={styles.sidebarItem}
-                onClick={() => {
-            // Use Next.js's router to programmatically navigate
-                const { useRouter } = require('next/router');
-                const router = useRouter();
-                router.push('/profile');
-            }}
-> 
+            <FontAwesomeIcon icon={faHome} className={styles.sidebarItem} />
+            <FontAwesomeIcon icon={faBell} className={styles.sidebarItem} />
+            <span
+                className={styles.sidebarItem}
+                onClick={() => router.push('/profile')}  // Use the function here
+            >
                 <FontAwesomeIcon icon={faUser} />
-                </span>
-
+            </span>
             <FontAwesomeIcon icon={faEnvelope} className={styles.sidebarItem} />
             <FontAwesomeIcon icon={faBookmark} className={styles.sidebarItem} />
             <FontAwesomeIcon icon={faCog} className={styles.sidebarItem}
-                onClick={toggleSettingsModal} />
+                             onClick={toggleSettingsModal} />
             <div className={styles.sidebarItem}>
-                <button className={styles.composeButton}>
+                <button className={styles.composeButton} onClick={() => router.push('/createPost')}>
                     <FontAwesomeIcon icon={faPen} />
                 </button>
             </div>
-            <SettingsModal 
-                show={isSettingsModalOpen} 
+            <SettingsModal
+                show={isSettingsModalOpen}
                 onClose={toggleSettingsModal}
             />
         </div>
