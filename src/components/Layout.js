fc5428df828cf00cf2a8653456aba7a01fa1d67b
Mia Mastoridis <miamastor@gmail.com>
2023-10-24 12:05:58 -0400

    Post UI
    Layout changes added editor

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,26 @@
 import SearchInput from "./rightnavbar/SearchInput";
-import Sidebar from "./Sidebar/Index";
+import Sidebar from "./Sidebar/index";
+import Followers from './Followers'
 
 export default function Layout({children}) {
   return (
+  
     <div className="grid grid-cols-12 max-w-screen mx-auto min-h-screen">
+  
     <div className="col-span-3 bg-black-200 h-full pr-3 text-zinc">
-      <Sidebar/>
+          <img
+          src="/white_logo_dark_background.png"
+          alt="Logo"
+          className="absolute top-0 w-1/5"
+          style={{left: '0rem' }}
+        />  
+          
       </div>
+
     <div className="col-span-5 bg-gray-200 h-full p-2 h-screen border-l border-r rl-stripe-bg">{children}</div>
-    
-    
+  
     <div className="col-span-4 bg-black-800 h-full pl-3 text-zinc">
-      <SearchInput />
+       <div><SearchInput /></div>
     </div>
  
   </div>
--- src/components/rightnavbar/SearchInput.js
+++ src/components/rightnavbar/SearchInput.js
@@ -18,14 +18,30 @@ const SearchInput = () => {
     };
 
     return (
-        <form onSubmit={onSearch} className="flex justify-center w-3/4">
+        <form onSubmit={onSearch} className="relative w-3/4">
             <input 
                 type = "text"
                 value={searchQuery || ""}
                 onChange={(event) => setSearchQuery(event.target.value)}
-                className="px-3 py-2sm:px-4 sm:py-3 flex text-zinc-200 bg-zinc-800 focus:bg-black rounded-full focus:outline-none focus:ring-[1px] focus:ring-purple-700 placeholder:text-zinc-400  w-full"
+                className="px-3 py-2sm:px-4 sm:py-3 flex text-zinc-200 bg-white focus:bg-black rounded-full focus:outline-none focus:ring-[1px] focus:ring-purple-700 placeholder:text-zinc-400  w-full"
                 placeholder="curiouser and curiouser..."
+                
             />
+            <button type="submit" className="absolute right-0 top-1 mt-3 mr-4">
+                <svg 
+                    className="w-4 h-4 fill-current"
+                    xmlns="http://www.w3.org/2000/svg" 
+                    viewBox="0 0 20 20" 
+
+                >
+                    <path 
+                        fillRule="evenodd" 
+                        d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" 
+                        clipRule="evenodd" 
+                    />
+                </svg>
+
+            </button>
         </form>
     );
 };
