1588e96d46426da11af6d7ca9df129ab12b6a992
Hayamabdalla <abdalla.hayam@yahoo.com>
2023-11-27 17:49:42 -0500

    new edit profile stuff

--- src/pages/editProfilePage.js
+++ src/pages/editProfilePage.js
@@ -1,5 +1,6 @@
+
 import Layout from "../components/Layout";
-import { useSession, getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -9,19 +10,28 @@ export default function EditProfile() {
 
     // State to store the profile form data
     const [profileData, setProfileData] = useState({
-        name: '',
+        username: '',
+        firstName: '',
+        lastName: '',
         email: '',
         bio: '',
+        password: '',
+        newPassword: ''
     });
+
+    // State to store any error messages
+    const [error, setError] = useState('');
 
     // Fetch the profile data when the component mounts
     useEffect(() => {
-        if (session) {
-            // Placeholder: Replace with your logic to fetch user profile
+        if (session?.user) {
+            // Initialize the profile data with session data or placeholders
             setProfileData({
-                name: 'John Doe', // Replace with data from session or fetch from API
-                email: 'john@example.com', // Replace with data from session or fetch from API
-                bio: 'This is a bio', // Replace with data from session or fetch from API
+                username: session.user.name || '',
+                firstName: 'John', // Placeholder: Replace with actual data
+                lastName: 'Doe', // Placeholder: Replace with actual data
+                email: session.user.email || '', // Placeholder: Replace with actual data
+                bio: 'This is a bio' // Placeholder: Replace with actual data
             });
         }
     }, [session]);
@@ -36,10 +46,37 @@ export default function EditProfile() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // Placeholder: Replace with your logic to submit profile data to API
-        console.log('Profile data submitted', profileData);
-        // Redirect after submit
-        router.push('/profile');
+        setError(''); // Clear previous error messages
+
+        try {
+            const response = await fetch('/api/userOperations/updateProfile', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Include any necessary headers such as an authentication token
+                },
+                body: JSON.stringify(profileData),
+            });
+
+            // Handle non-JSON response
+            if (!response.ok) {
+                if (response.headers.get("Content-Type")?.includes("application/json")) {
+                    const errorData = await response.json();
+                    throw new Error(errorData.error || 'Failed to update profile');
+                } else {
+                    // Response is not JSON (e.g., server error page)
+                    const errorText = await response.text();
+                    throw new Error(errorText || 'Failed to update profile');
+                }
+            }
+
+            const data = await response.json();
+            console.log('Profile updated successfully', data);
+            router.push('/profile'); // Redirect to the profile page
+        } catch (error) {
+            console.error('An error occurred while updating the profile:', error);
+            setError(error.message || 'An error occurred');
+        }
     };
 
     if (!session) {
@@ -49,47 +86,98 @@ export default function EditProfile() {
     return (
         <Layout>
             <div className="container mx-auto p-4">
-                 {/* Back Arrow */}
-                
-                <button onClick={() => router.push('/ViewProfile')}>← Back</button>
+                <button onClick={() => router.push('/ViewProfile')} className="text-white">
+                    ← Back
+                </button>
                 <h1 className="text-xl font-bold text-white mb-4">Edit Profile</h1>
+                {error && <div className="text-red-500 mb-4">{error}</div>}
                 <form onSubmit={handleSubmit} className="space-y-4">
+                    {/* Form fields for editing the profile */}
+                    {/* ... existing form fields ... */}
                     <div>
-                        <label htmlFor="name" className="text-white">Name</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            value={profileData.name}
-                            onChange={handleChange}
-                            className="block w-full mt-1"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email" className="text-white">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={profileData.email}
-                            onChange={handleChange}
-                            className="block w-full mt-1"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="bio" className="text-white">Bio</label>
-                        <textarea
-                            id="bio"
-                            name="bio"
-                            value={profileData.bio}
-                            onChange={handleChange}
-                            rows="4"
-                            className="block w-full mt-1"
-                        />
-                    </div>
-                    <button type="submit" className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700">Save Changes</button>
+<label htmlFor="username" className="text-white">Username</label>
+<input
+    type="text"
+    id="username"
+    name="username"
+    value={profileData.username}
+    onChange={handleChange}
+    className="block w-full mt-1"
+/>
+</div>
+<div>
+<label htmlFor="firstName" className="text-white">First Name</label>
+<input
+    type="text"
+    id="firstName"
+    name="firstName"
+    value={profileData.firstName}
+    onChange={handleChange}
+    className="block w-full mt-1"
+/>
+</div>
+<div>
+<label htmlFor="lastName" className="text-white">Last Name</label>
+<input
+    type="text"
+    id="lastName"
+    name="lastName"
+    value={profileData.lastName}
+    onChange={handleChange}
+    className="block w-full mt-1"
+/>
+</div>
+<div>
+<label htmlFor="email" className="text-white">Email</label>
+<input
+    type="email"
+    id="email"
+    name="email"
+    value={profileData.email}
+    onChange={handleChange}
+    className="block w-full mt-1"
+/>
+</div>
+<div>
+<label htmlFor="bio" className="text-white">Bio</label>
+<textarea
+    id="bio"
+    name="bio"
+    value={profileData.bio}
+    onChange={handleChange}
+    rows="4"
+    className="block w-full mt-1"
+/>
+</div>
+<div>
+<label htmlFor="password" className="text-white">Current Password</label>
+<input
+    type="password"
+    id="password"
+    name="password"
+    value={profileData.password}
+    onChange={handleChange}
+    className="block w-full mt-1"
+/>
+</div>
+<div>
+<label htmlFor="newPassword" className="text-white">New Password</label>
+<input
+    type="password"
+    id="newPassword"
+    name="newPassword"
+    value={profileData.newPassword}
+    onChange={handleChange}
+    className="block w-full mt-1"
+/>
+</div>
+
+                    <button type="submit" className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700">
+                        Save Changes
+                    </button>
                 </form>
             </div>
         </Layout>
     );
 }
+
