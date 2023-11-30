faafc30ffc5caa687bb8e8d2a5683e43d4f9542e
Dobson Dunavant <dobsonddev@gmail.com>
2023-11-29 20:57:15 -0500

    Update createPost.js

--- src/pages/createPost.js
+++ src/pages/createPost.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import TextEditor from '@/components/CreatePost/TextEditor';
@@ -7,6 +7,14 @@ import handlePostClick from '@/components/CreatePost/TextEditor';
 export default function CreatePost() {
 	const { data: session } = useSession();
 	const router = useRouter();
+	const [userId, setUserId] = useState(null);
+
+	useEffect(() => {
+		if (session?.token?.sub) {
+			setUserId(session.token.sub);
+		}
+		console.log('Session token in profile.js:', userId);
+	}, [session]);
 
 	useEffect(() => {
 		if (!session) {
@@ -18,7 +26,7 @@ export default function CreatePost() {
 		<div style={{ backgroundColor: 'white', color: 'black', height: '100vh', padding: '20px' }}>
 			<h1>Create a Post</h1>
 			{session ? (
-				<TextEditor />
+				<TextEditor userId={userId}/>
 			) : (
 				<p>Loading...</p> // This will be shown briefly before the user is redirected
 			)}
